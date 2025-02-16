9e5c269945929ad9a58a3352413f8434cedca16a
Kasualix <670067575@qq.com>
2025-02-16 14:24:16 +0800

    complete

--- common/src/main/java/com/teampotato/finaldragon/FinalDragon.java
+++ common/src/main/java/com/teampotato/finaldragon/FinalDragon.java
@@ -4,11 +4,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.teampotato.finaldragon.enchantment.FinalPower;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,30 +14,30 @@ import java.io.*;
 public final class FinalDragon {
     public static final String MOD_ID = "finaldragon";
     public static boolean disableShield;
-    public static final Enchantment FINAL_POWER = Registry.register(BuiltInRegistries.ENCHANTMENT, FinalPower.ID, new FinalPower());
 
     public static boolean entityHaveFinalPower(@NotNull LivingEntity livingSource) {
         ItemStack mainHand = livingSource.getMainHandItem();
         ItemStack offHand = livingSource.getOffhandItem();
-        return EnchantmentHelper.getItemEnchantmentLevel(FINAL_POWER, mainHand) != 0 || EnchantmentHelper.getItemEnchantmentLevel(FINAL_POWER, offHand) != 0;
+        return EnchantmentHelper.getItemEnchantmentLevel(FinalPower.INSTANCE, mainHand) != 0 || EnchantmentHelper.getItemEnchantmentLevel(FinalPower.INSTANCE, offHand) != 0;
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void init() {
+        FinalPower.register();
         File configPath = FabricLoader.getInstance().getConfigDir().toFile();
-        if(configPath.mkdirs()) {
-            File config = new File(configPath, MOD_ID + ".json");
-            if (!config.exists()) {
-                try {
-                    FileWriter writer = writeFile(config);
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+        configPath.mkdirs();
+        File config = new File(configPath, MOD_ID + ".json");
+        if (!config.exists()) {
+            try {
+                FileWriter writer = writeFile(config);
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(configPath));
+            BufferedReader reader = new BufferedReader(new FileReader(config));
             disableShield = JsonParser.parseReader(reader).getAsJsonObject().get("WhetherFinalPowerIgnoresShields").getAsBoolean();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
--- common/src/main/java/com/teampotato/finaldragon/enchantment/FinalPower.java
+++ common/src/main/java/com/teampotato/finaldragon/enchantment/FinalPower.java
@@ -1,13 +1,19 @@
 package com.teampotato.finaldragon.enchantment;
 
 import com.teampotato.finaldragon.FinalDragon;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class FinalPower extends Enchantment {
-    public static final ResourceLocation ID = new ResourceLocation(FinalDragon.MOD_ID, "final_power");
+    public static final Enchantment INSTANCE = new FinalPower();
+
+    public static void register() {
+        Registry.register(BuiltInRegistries.ENCHANTMENT, new ResourceLocation(FinalDragon.MOD_ID, "final_power"), INSTANCE);
+    }
 
     public FinalPower() {
         super(Rarity.VERY_RARE, EnchantmentCategory.BREAKABLE, new EquipmentSlot[]{EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND});
